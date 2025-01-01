ab0a68240b52882c5457bd16a2bbbef97629da65
ahmadowais19 <ahmadelian795@gmail.com>
2025-01-01 23:50:50 +0300

    Add files via upload

---
+++ task.js
@@ -0,0 +1,66 @@
+
+// task1 
+for (let i =1; i<= 10; i++)
+    document.writeln(i);
+
+document.writeln("<br>")
+ // 2
+
+let numbers = 0;
+while(numbers <=10){
+    document.writeln(numbers);
+    numbers ++;
+}
+document.writeln("<br>")
+// 3
+let i = [1,2,3,4,5,6,7,8,9,10];
+for (let i = 0 ; i < i.length; i++);
+    document.writeln([i]);
+
+    document.writeln("<br>")
+
+// 4
+for(let i =0 ; i<=10 ; i++)
+    if(i % 2 == 0)
+    document.writeln(i)
+
+document.writeln("<br>")
+
+    
+// 5
+
+let total = 0
+for(let i =0 ; i <= 10; i++){
+    total +=i;
+}
+document.writeln(total)
+
+
+document.writeln("<br>")
+
+
+// // 6
+// let i =[1,2,3,4,5];
+// let largest = 0;
+// for(iet i = 1 ; i<i.length ; i++) {
+//     if(numbers[i]>largest) { 
+//         largest = numbers [i];
+//     }
+// }
+// document.writeln(largest);
+
+// document.writeln("<br>")
+
+// 7
+
+
+
+
+// 8
+let number=[5,4,3,2,1]
+let  a = 0
+for(let i= 0; i< number;i++){
+a += number [i]
+}
+let average = a / number.length;
+document.writeln[average];
